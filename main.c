a158a956e071ab65d518cc1ebf5348f47f8af2cb
Miguel Muscat <miguelmuscat93@gmail.com>
2017-12-15 22:53:12 +0100

    Added usage example

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include "src/hashmap.h"
 
 int main()
 {
-    printf("Hello, World!\n");
+    Hashmap *map = hashmap_new(10);
+    hashmap_set(map, "name", "andre");
+    hashmap_set(map, "surname", "jenkins");
+    hashmap_set(map, "career", "software developer");
+    hashmap_set(map, "institute", "uom");
+    hashmap_set(map, "cousin", "miguel muscat");
+    hashmap_set(map, "memelevel", "0");
+    hashmap_set(map, "memelevel", "999");
+
+    hashmap_remove(map, "cousin");
+
+    for (unsigned int i = 0; i < map->size; ++i) {
+        printf("Index #%d [", i);
+
+        ItemList *list = &(map->lists[i]);
+
+        if (list != NULL && list->item != NULL) {
+            while (list != NULL && list->item != NULL) {
+                char *key = list->item->key;
+                printf("\n\t(%s) %s", key, hashmap_get(map, key));
+                list = list->next;
+            }
+            printf("\n");
+        }
+
+        printf("]\n");
+    }
+
     return 0;
 }
